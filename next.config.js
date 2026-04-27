28801e7a8d49fb1a5ed0b3bb5b17c29415c26438
rickdynamix <rick11.cricket@gmail.com>
2026-04-27 15:13:47 +0530

    fixed vercel error

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com'],
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 }
 
